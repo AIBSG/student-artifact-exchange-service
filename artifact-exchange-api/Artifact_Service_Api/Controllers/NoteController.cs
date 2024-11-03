25493b0af833d107362c6bb84c3d11461789d6f7
Нургатин Марат Маратович <nurgatin2000@mail.ru>
2024-11-03 15:12:13 +0500

    Сделано миграцию для NoteFile.

--- artifact-exchange-api/Artifact_Service_Api/Controllers/NoteController.cs
+++ artifact-exchange-api/Artifact_Service_Api/Controllers/NoteController.cs
@@ -11,6 +11,8 @@ namespace Artifact_Service_Api.Controllers;
 public class NoteController(AppDbContext context) : ControllerBase
 {
     private readonly AppDbContext _context = context;
+    private readonly EditNoteRequest _request;
+    
 
     [HttpGet]
     public async Task<IEnumerable<Note>> GetOpenNotes() =>
@@ -25,11 +27,13 @@ public class NoteController(AppDbContext context) : ControllerBase
     public async Task<IActionResult> GetNote(Guid id)
     {
         var note = await _context.Notes
+            .Where(n => n.Id.Equals(id))
+            .Include(n => n.NoteAccess)
             .Include(n => n.NoteTags)
             .Include(n => n.Author)
             .Include(n => n.Files)
-            .Include(n => n.NoteAccess)
-            .FirstOrDefaultAsync(n => n.Id == id);
+            .Select(n => n.Files)
+            .FirstOrDefaultAsync();
         if (note == null) return NotFound();
         return Ok(note);
     }
@@ -80,9 +84,66 @@ public class NoteController(AppDbContext context) : ControllerBase
                 CustomFileName = file.FileName,
                 ServerFileName = serverFileName
             });
-        } 
+        }
+        note.NoteAccess.Add(new NoteAccess {
+            UserId = note.Author.Id,
+            NoteId = note.Id,
+            CanEdit = true
+        });
+
         await _context.Notes.AddAsync(note);
         await _context.SaveChangesAsync();
-        return Ok(note);
+        return Created();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetNoteRequest(Guid id, Guid userId)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if (note == null) return NotFound();
+
+        var userAccess = await _context.NoteAccesses.Where(n => n.NoteId == id && n.UserId == userId).FirstOrDefaultAsync();
+        if (!userAccess.CanEdit) return Forbid();
+        
+        _request.AuthorId = note.Author.Id;
+        _request.NoteId = note.Id;
+        _request.Title = note.Title;
+        _request.Description = note.Description;
+        _request.Text = note.Text;
+        _request.IsOpen = note.IsOpen;
+        _request.TagsNames = await _context.NoteTags.Where(nt => nt.NoteId == id).Select(nt => nt.Tag.Name).ToListAsync();
+
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> EditNote(Guid id, SaveNoteRequest request)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if (note == null) return NotFound();
+
+        request.Title = note.Title;
+        request.Description = note.Description;
+        request.Text = note.Text;
+        request.IsOpen = note.IsOpen;
+        request.TagsNames = await _context.NoteTags.Where(nt => nt.NoteId == id).Select(nt => nt.Tag.Name).ToListAsync();
+
+        note.Title = request.Title;
+        note.Description = request.Description;
+        note.Text = request.Text;
+        note.IsOpen = request.IsOpen;
+
+        return Ok();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteNote(Guid id)
+    {
+        var note = await _context.Notes.FindAsync(id);
+        if (note == null) return NotFound();
+
+        _context.Notes.Remove(note);
+        await _context.SaveChangesAsync();
+        return Ok();
     }
 }
---
+++ artifact-exchange-api/Artifact_Service_Api/Dtos/EditNoteRequest.cs
@@ -0,0 +1,18 @@
+using Artifact_Service_Api.Models;
+
+namespace Artifact_Service_Api.Dtos;
+
+public class EditNoteRequest
+{
+    public Guid NoteId { get; set; }
+    public Guid AuthorId { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Text { get; set; }
+    public bool IsOpen { get; set; }
+    public IEnumerable<string>? TagsNames { get; set; }
+    public IEnumerable<string>? FilesNames { get; set; }
+    public IEnumerable<IFormFile>? Files { get; set; }
+    public IEnumerable<string>? Emails { get; set; }
+    public IEnumerable<bool>? CanEdit { get; set; }
+}
