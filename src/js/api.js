5398a1b16991346d78d9cb8ad4031a95187d07c0
Илья <ilya.panarin14@gmail.com>
2024-12-01 18:06:36 +0500

    Поправил отображение почты пользователя в Sidebar

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
 const API_SHARE_URL = 'http://localhost:5182/swagger/api/share'; // URL для отправки данных в окне Поделиться
 
-const API_FILE_CREATE_URL = 'http://localhost:5182/swagger/api/file-create'; // URL для отправки данных файла
+const API_FILE_CREATE_URL = 'http://localhost:5182/swagger/api/Document/SaveNewDocument'; // URL для отправки данных файла
 
 const API_FILE_EDIT_URL = 'http://localhost:5182/swagger/api/file-edit';
 
